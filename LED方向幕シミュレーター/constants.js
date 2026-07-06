935ee7db1a2b383a0bb882bad1dcb67eaac3e8f5
led <yukkicha228@gmail.com>
2026-07-06 16:41:58 +0900

    Delete LED方向幕シミュレーター directory

--- LED方向幕シミュレーター/constants.js
+++
@@ -1,8 +0,0 @@
-const destinationWidth = 81;
-const matrixWidth = 128;
-const matrixHeight = 32;
-
-const ledsize = 6;
-const ledgap = 1;
-const pitch = ledsize + ledgap;
-const radius = ledsize / 2;
